e27427b9b5721948c8c232ca8d0ffbb3aceb3dba
Zëv <90863634+Zev07@users.noreply.github.com>
2026-02-08 18:13:40 -0300

    Feat: Solucionando erro no arquivo image.ts

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,9 +1,10 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { client } from './client'
-import type { SanityImageSource } from '@sanity/image-url/lib/types/types'
+
+import type { SanityImageSource } from 'sanity'
 
 const builder = createImageUrlBuilder(client)
 
-export function urlFor(source: SanityImageSource) {
+export const urlFor = (source: SanityImageSource) => {
   return builder.image(source)
 }
